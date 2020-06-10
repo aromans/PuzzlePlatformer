836e1c7371385840be9e2847ab1dc95efb590b2d
aromans <andrew.romans@hotmail.com>
2020-06-10 18:37:56 -0400

    Changed file type to .glsl

---
+++ Shaders/fShader.glsl
@@ -0,0 +1,28 @@
+#version 460						 
+							
+in vec4 vColor;
+in vec2 TexCoord;
+in vec3 Normal;
+		 
+out vec4 color;
+
+struct DLight 
+{
+	vec3 color;
+	float ambientIntensity;
+	vec3 direction;
+	float diffuseIntensity;
+};
+
+uniform sampler2D theTexture;
+uniform DLight directionalLight;
+									 
+void main() 
+{
+	vec4 ambientColor = vec4(directionalLight.color, 1.0f) * directionalLight.ambientIntensity;
+
+	float diffuseFactor = max(dot(normalize(Normal), normalize(directionalLight.direction)), 0.0f);
+	vec4 diffuseColor = vec4(directionalLight.color, 1.0f) * directionalLight.diffuseIntensity * diffuseFactor;
+
+	color = texture(theTexture, TexCoord) * (ambientColor + diffuseColor);
+}
