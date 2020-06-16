bff6eace1f1862263901caba59b46fad8d5cb061
aromans <andrew.romans@hotmail.com>
2020-06-16 03:11:40 -0400

    Fixed Lighting issues - added Shadow Map

--- Shaders/normalFShader.glsl
+++ Shaders/normalFShader.glsl
@@ -42,11 +42,15 @@ struct Material
 	float shininess;
 };
 
-in vec3 v_Position;
-in vec2 v_TexCoord;
-in vec3 v_Normal;
-in vec3 v_Tangent;
-in vec3 v_Bitangent;
+
+in VS_OUT
+{
+	vec3 v_Position;
+	vec3 v_Normal;
+	vec2 v_TexCoord;
+} fs_in;
+
+in vec4 DirectionalLightSpacePos;
 
 out vec4 out_color;
 
@@ -61,96 +65,136 @@ uniform PointLight pointLights[MAX_POINT_LIGHTS];
 uniform SpotLight spotLights[MAX_SPOT_LIGHTS];
 uniform DirectionalLight directionalLight;
 
+// Shadow Map
+uniform sampler2D directionalShadowMap;
+
 uniform Material material;
 uniform vec3 cameraPos;
 
-vec4 CalcLight(Light light, vec3 direction, vec3 normal)
+
+float CalcDirectionalShadowFactor(vec3 direction, vec3 normal)
 {
-	vec4 ambient = vec4(light.color, 1.0f) * light.ambientIntensity;
-	float diffuseFactor = max(dot(normalize(direction), normalize(normal)), 0.0f);
+	vec3 projCoords = DirectionalLightSpacePos.xyz / DirectionalLightSpacePos.w;
+	projCoords = (projCoords * 0.5) + 0.5;
 
-	vec4 diffuse = vec4(light.color, 1.0f) * light.diffuseIntensity * diffuseFactor;
+	float current = projCoords.z;
 
-	vec4 specular = vec4(0, 0, 0, 0);
-	if (diffuseFactor > 0.0f)
-	{ 
-		vec3 VertexToEye = normalize(cameraPos - v_Position);
-		vec3 Reflection = normalize(reflect(-direction, normalize(normal)));
+	vec3 Normal = normalize(normal);
+	vec3 lightDir = normalize(direction);
 
-		float specularFactor = dot(Reflection, VertexToEye);
+	float bias = max(0.05 * (1.0 - dot(lightDir, Normal)), 0.005);
 
-		if (specularFactor > 0.0f)
-		{
-			specularFactor = pow(clamp(specularFactor, 0.0f, 1.0f), material.shininess);
-			specular = vec4(light.color * material.specular * specularFactor, 1.0f);
+	float shadow = 0.0f;
+
+	int shadowDetail = 1;
+
+	vec2 texelSize = 1.0 / textureSize(directionalShadowMap, 0);
+	for (int x = -1; x <= 1; ++x) {
+		for (int y = -1; y <= 1; ++y) {
+			float pcfDepth = texture(directionalShadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
+			shadow += current - bias > pcfDepth ? 1.0 : 0.0;
 		}
 	}
 
-	return (ambient + diffuse + specular);
+	shadow /= 9;
+
+	if (projCoords.z > 1.0) {
+		shadow = 0.0;
+	}
+
+	return shadow;
+}
+
+vec4 CalcLight(Light light, vec3 direction, vec3 normal, float shadowFactor)
+{
+	vec4 ambientColor = vec4(light.color, 1.0) * light.ambientIntensity;
+
+	float diffuseFactor = max(dot(normalize(direction), normalize(normal)), 0.0);
+
+	vec4 diffuseColor = vec4(0, 0, 0, 0);
+	vec4 specularColor = vec4(0, 0, 0, 0);
+
+	if (diffuseFactor > 0.0)
+	{
+		diffuseColor = vec4(light.color * light.diffuseIntensity * diffuseFactor, 1.0);
+
+		vec3 toEyePos = normalize(cameraPos - fs_in.v_Position);
+		vec3 reflection = normalize(reflect(-normalize(direction), normalize(normal)));
+
+		float specularFactor = dot(reflection, toEyePos);
+
+		if (specularFactor > 0.0)
+		{
+			specularFactor = pow(specularFactor, material.shininess);
+			specularColor = vec4(light.color * material.specular * specularFactor, 1.0);
+		}
+	}
+
+	return (ambientColor + ((1.0 - shadowFactor) * (diffuseColor + specularColor)));
 }
 
 vec4 CalcPointLight(PointLight light, vec3 normal)
 {
-	vec3 lightDir = v_Position - light.position;
-	float distance = length(lightDir);
-	lightDir = normalize(lightDir);
+	vec3 toLightDir = fs_in.v_Position - light.position;
+	float distance = length(toLightDir);
+	toLightDir = normalize(toLightDir);
 
-	vec4 Color = CalcLight(light.base, lightDir, normal);
-	float attenuation = light.constant + 
+	vec4 final_color = CalcLight(light.base, toLightDir, normal, 0.0);
+
+	float attenuation = light.constant +
 						(light.linear * distance) +
 						(light.exponent * distance * distance);
 
-	return Color / attenuation;
+	return final_color / attenuation;
 }
 
 vec4 CalcSpotLight(SpotLight light, vec3 normal)
 {
-	vec3 direction = normalize(v_Position - light.base.position);
-	float lightFactor = dot(light.direction, direction);
+	vec3 toLightDir = normalize(fs_in.v_Position - light.base.position);
+	float lightFactor = dot(normalize(light.direction), toLightDir);
 
-	if (lightFactor > light.edge) { 
-		vec4 Color = CalcPointLight(light.base, normal);
-		return Color * (1.0f - (1.0f - lightFactor) * 1.0f / (1.0f - light.edge));
-	} else {
-		return vec4(0,0,0,0);
+	if (lightFactor > light.edge) {
+		vec4 final_color = CalcPointLight(light.base, normal);
+		return final_color * (1.0f - (1.0f - lightFactor) * 1.0f / (1.0f - light.edge));
+	} else { 
+		return vec4(0, 0, 0, 0);
 	}
 }
 
-vec3 CalcBumpedNormal()
+/*
+vec3 CalcNewNormal()
 {
-	if (material.has_normal_map == 0) return normalize(v_Normal);
-
-	vec3 Normal = normalize(v_Normal);
-	vec3 Tangent = normalize(v_Tangent);
-	vec3 Bitangent = normalize(v_Bitangent);
-	vec3 BumpMapNormal = (texture(material.normal, v_TexCoord)).xyz;
 	vec3 NewNormal;
-	mat3 TBN = mat3(Tangent, Bitangent, Normal);
-	NewNormal = TBN * BumpMapNormal;
-	NewNormal = normalize(NewNormal);
+	if (material.has_normal_map != 0) {
+		vec3 BumpMapNormal = (texture(material.normal, fs_in.v_TexCoord)).rgb;
+		BumpMapNormal = BumpMapNormal * 2.0 - 1.0;
+		NewNormal = normalize(fs_in.TBN * BumpMapNormal);
+	} else {
+		NewNormal = fs_in.v_PassedNormal;
+	}
 	return NewNormal;
-}
+}*/
 
 void main()
 {
-	vec3 Normal = CalcBumpedNormal();
+	vec3 Normal = fs_in.v_Normal; //CalcNewNormal();
 
-	vec4 light_color = CalcLight(directionalLight.base, directionalLight.direction, Normal);
+	float shadowFactor = CalcDirectionalShadowFactor(directionalLight.direction, Normal);
 
-	for (int i = 0; i < pointLightCount; ++i)
-	{
+	vec4 light_color = CalcLight(directionalLight.base, directionalLight.direction, Normal, shadowFactor);
+
+	for (int i = 0; i < pointLightCount; ++i) {
 		light_color += CalcPointLight(pointLights[i], Normal);
 	}
 
-	for (int i = 0; i < spotLightCount; ++i) 
-	{
+	for (int i = 0; i < spotLightCount; ++i) {
 		light_color += CalcSpotLight(spotLights[i], Normal);
 	}
 
-	vec4 frag_color = texture(material.diffuse, v_TexCoord);
+	vec4 frag_color = texture(material.diffuse, fs_in.v_TexCoord);
 
 	if (frag_color.a < 0.5)
 		discard;
 
-	out_color = texture(material.diffuse, v_TexCoord) * light_color;
+	out_color = frag_color * light_color;
 }
--- Shaders/normalVShader.glsl
+++ Shaders/normalVShader.glsl
@@ -6,27 +6,38 @@ layout (location = 2) in vec3 normal;
 layout (location = 3) in vec3 tangent;
 layout (location = 4) in vec3 bitangent;
 
-out vec3 v_Position;
-out vec2 v_TexCoord;
+/*out vec3 v_Position;
 out vec3 v_Normal;
 out vec3 v_Tangent;
 out vec3 v_Bitangent;
+out vec3 v_PassedNormal;
+
+out vec3 E;*/
+
+out VS_OUT
+{
+	vec3 v_Position;
+	vec3 v_Normal;
+	vec2 v_TexCoord;
+} vs_out;
+
+out vec4 DirectionalLightSpacePos;
 
 uniform mat4 MVP;
 uniform mat4 V;
 uniform mat4 M;
+uniform mat4 directionalLightTransform;
+uniform vec3 cameraPos;
 
 void main()
 {
 	gl_Position = MVP * vec4(pos, 1.0);
 
-	v_Position = (M * vec4(pos, 1.0)).xyz;
+	vs_out.v_Position = (M * vec4(pos, 1.0)).xyz;
 
-	v_TexCoord = texcoord;
+	vs_out.v_Normal = mat3(transpose(inverse(M))) * normal;
 
-	v_Tangent = (M * vec4(tangent, 1.0)).xyz;
+	vs_out.v_TexCoord = texcoord;
 
-	v_Bitangent = (M * vec4(bitangent, 1.0)).xyz;
-
-	v_Normal = (M * vec4(normal, 1.0)).xyz;
+	DirectionalLightSpacePos = directionalLightTransform * M * vec4(pos, 1.0);
 }
