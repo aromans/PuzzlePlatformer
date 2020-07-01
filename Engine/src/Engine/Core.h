f0c09b0f48cb30d9575cbbd3384cff1e9d216b53
aromans <andrew.romans@hotmail.com>
2020-07-01 10:06:45 -0400

    Running Static Library

--- Engine/src/Engine/Core.h
+++ Engine/src/Engine/Core.h
@@ -1,18 +1,19 @@
 #pragma once
 
-#ifdef ENG_PLATFORM_WINDOWS
-#if ENG_DYNAMIC_LINK
-	#ifdef ENG_BUILD_DLL
-		#define ENG_API __declspec(dllexport)
-	#else
-		#define ENG_API __declspec(dllimport)
-	#endif
-#else
-	#define ENG_API
-#endif
-#else 
-	#error Engine Only Supports Windows!
-#endif
+#define ENG_API
+//#ifdef ENG_PLATFORM_WINDOWS
+//#if ENG_DYNAMIC_LINK
+//	#ifdef ENG_BUILD_DLL
+//		#define ENG_API __declspec(dllexport)
+//	#else
+//		#define ENG_API __declspec(dllimport)
+//	#endif
+//#else
+//
+//#endif
+//#else 
+//	#error Engine Only Supports Windows!
+//#endif
 
 #define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
 #define BIND_EVENT_FN_TO(x,y) std::bind(&x, y, std::placeholders::_1)
