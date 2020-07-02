936072a13fd713a0ef4982174d5fa2fbf95caa85
aromans <andrew.romans@hotmail.com>
2020-07-01 20:49:45 -0400

    Added Precompiled Header

---
+++ Engine/src/Epch.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//#ifdef ENG_PLATFORM_WINDOWs
+////#include "Window.h"
+//#endif
+
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <memory>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+#include <array>
+#include <utility>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+// Maths!
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+#include <Engine/Core/Log.h>
+//#include <Engine/Core/Events/Event.h>
+
+// Parsers
+//#include "..\Parsers\pugixml-1.10\pugiconfig.hpp"
+//#include "..\Parsers\pugixml-1.10\pugixml.hpp"
