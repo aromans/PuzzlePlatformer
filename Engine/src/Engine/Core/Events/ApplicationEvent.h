843da4b12cd7c0b02186db496bc566c143be285b
aromans <andrew.romans@hotmail.com>
2020-07-01 21:20:20 -0400

    Re-Added and Refactored Event Types and Dispatcher

---
+++ Engine/src/Engine/Core/Events/ApplicationEvent.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Epch.h"
+#include "Event.h"
+
+namespace Engine {
+
+	class WindowResizeEvent : public Event {
+	public:
+		WindowResizeEvent(unsigned int width, unsigned int height)
+			: m_Width(width), m_Height(height) { }
+
+		inline unsigned int GetWidth() const { return m_Width; }
+		inline unsigned int GetHeight() const { return m_Height; }
+
+		std::string ToString() const override {
+			std::stringstream s;
+			s << "WindowResizedEvent: Width-> " << m_Width << " | Height-> " << m_Height;
+			return s.str();
+		}
+
+		EVENT_CLASS_TYPE(WindowResize);
+		EVENT_CLASS_CATEGORY(EventCategoryApplication);
+	private:
+		unsigned int m_Width, m_Height;
+	};
+
+	class WindowCloseEvent : public Event {
+	public:
+		WindowCloseEvent() { }
+
+		EVENT_CLASS_TYPE(WindowClose);
+		EVENT_CLASS_CATEGORY(EventCategoryApplication);
+	};
+
+	class OnTickEvent : public Event {
+	public:
+		OnTickEvent() { }
+
+		EVENT_CLASS_TYPE(OnTick);
+		EVENT_CLASS_CATEGORY(EventCategoryApplication);
+	};
+
+	class OnUpdateEvent : public Event {
+	public:
+		OnUpdateEvent() { }
+
+		EVENT_CLASS_TYPE(OnUpdate);
+		EVENT_CLASS_CATEGORY(EventCategoryApplication);
+	};
+
+	class OnRenderEvent : public Event {
+	public:
+		OnRenderEvent() { }
+
+		EVENT_CLASS_TYPE(OnRender);
+		EVENT_CLASS_CATEGORY(EventCategoryApplication);
+	};
+
+}
---
+++ Engine/src/Engine/Core/Events/KeyEvent.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "Epch.h"
+#include "Event.h"
+
+namespace Engine {
+
+	class KeyEvent : public Event {
+	public:
+		inline int GetKeyButton() const { return m_Key; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput);
+	protected:
+		KeyEvent(int key) : m_Key(key) { }
+
+		int m_Key;
+	};
+
+	class KeyPressedEvent : public KeyEvent {
+	public:
+		KeyPressedEvent(int key, int repeatCount)
+			: KeyEvent(key), m_RepeatCount(repeatCount) { }
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override {
+			std::stringstream s;
+			s << "KeyPressedEvent: " << m_Key << " | " << m_RepeatCount << " repeats";
+			return s.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed);
+	protected:
+		int m_RepeatCount;
+	};
+
+	class KeyReleasedEvent : public KeyEvent {
+	public:
+		KeyReleasedEvent(int key) : KeyEvent(key) { }
+
+		std::string ToString() const override {
+			std::stringstream s;
+			s << "KeyReleasedEvent: " << m_Key;
+			return s.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased);
+	};
+
+	class KeyTypedEvent : public KeyEvent {
+	public:
+		KeyTypedEvent(int key) : KeyEvent(key) { }
+
+		std::string ToString() const override {
+			std::stringstream s;
+			s << "KeyTypedEvent: " << m_Key;
+			return s.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped);
+	};
+
+}
